b7f02673980a4cc335488bc66b6190d3a89416de
Travis Ricks <travis@travisricks.com>
2020-03-14 12:32:50 +0900

    remove console.log statements

--- fcc-basic-algorithms/04-find-longest-word/spec.js
+++ fcc-basic-algorithms/04-find-longest-word/spec.js
@@ -1,14 +1,10 @@
 const { expect } = require("chai");
 const algorithm = require(".");
 
-describe.only("find the longest string", () => {
-  console.log('start');
+describe("find the longest string", () => {
   it("should return the expected result", () => {
-    console.log('a', algorithm("this thisssss"));
     const result = algorithm("The quick brown fox jumped over the lazy dog");
-
-    console.log('result', result)
-
+    
     expect(result).to.deep.equal(6);
   });
 });
